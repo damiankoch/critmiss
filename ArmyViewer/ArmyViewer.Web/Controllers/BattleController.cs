fdd66e3c4921c74634334d444b2370e7aefba860
Andrew Petz <andrewwpetz@gmail.com>
2019-08-07 14:35:01 -0500

    Fixed Bootstrap layout

--- ArmyViewer/ArmyViewer.Web/Controllers/BattleController.cs
+++ ArmyViewer/ArmyViewer.Web/Controllers/BattleController.cs
@@ -24,10 +24,29 @@ namespace ArmyViewer.Web.Controllers
                 CreatorYTProfile = "https://youtube.com/DudePerfect",
                 Description = "Test description",
                 Name = "Test Battle",
-                YTLink = "https://www.youtube.com/watch?v=9f_k7_P3-nA"
+                YTLink = newUrl
             };
 
             return View(battle);
         }
+
+        //public ActionResult Index(int Id)
+        //{
+        //    //ApplicationDbContext context = new ApplicationDbContext();
+
+        //    string newUrl = Battle.ProcessYouTubeLink("https://www.youtube.com/watch?v=9f_k7_P3-nA");
+
+        //    BattleViewModel battle = new BattleViewModel
+        //    {
+        //        CreatorId = 1,
+        //        CreatorName = "Andrew",
+        //        CreatorYTProfile = "https://youtube.com/DudePerfect",
+        //        Description = "Test description",
+        //        Name = "Test Battle",
+        //        YTLink = newUrl
+        //    };
+
+        //    return View(battle);
+        //}
     }
 }
